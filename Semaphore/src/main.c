e46aeef6896ce19a4eb1f3bd4ae19a60dd6c17b8
DESKTOP-VDE9A7I\Utilisateur <ngdai6789@gmail.com>
2019-01-26 12:29:00 -0500

    My code but not work with segger

--- Semaphore/src/main.c
+++ Semaphore/src/main.c
@@ -7,180 +7,134 @@
   * @brief   Default main function.
   ******************************************************************************
 */
-#include<stdio.h>
-#include<stdint.h>
-#include<string.h>
-#include "stm32f4xx.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
+#include "stm32f4xx.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "FreeRTOSConfig.h"
+#include "semphr.h"
+TaskHandle_t xTaskHandle_1=NULL;
+TaskHandle_t xTaskHandle_2=NULL;
+void vTask1(void *params);
+void vTask2(void *params);
 
-
-TaskHandle_t xTaskHandle1=NULL;
-TaskHandle_t xTaskHandle2=NULL;
-
-//Task functions prototypes
-void vTask1_handler(void *params);
-void vTask2_handler(void *params);
+SemaphoreHandle_t xSemaphore=NULL;
 
 #ifdef USE_SEMIHOSTING
-//used for semihosting
 extern void initialise_monitor_handles();
 #endif
 
-static void prvSetupHardware(void);
-static void prvSetupUart(void);
+static void prvSetupHardware();
 void printmsg(char *msg);
-
-//some macros
-#define TRUE 1
-#define FALSE 0
-#define AVAILABLE TRUE
-#define NOT_AVAILABLE FALSE
-
-//Global variable section
-char usr_msg[250]={0};
-uint8_t UART_ACCESS_KEY = AVAILABLE;
-
-int main(void)
-{
-
-#ifdef USE_SEMIHOSTING
-	initialise_monitor_handles();
-	printf("This is hello world example code\n");
-#endif
-
-	DWT->CTRL |= (1 << 0);//Enable CYCCNT in DWT_CTRL.
-
-	//1.  Reset the RCC clock configuration to the default reset state.
-	//HSI ON, PLL OFF, HSE OFF, system clock = 16MHz, cpu_clock = 16MHz
-	RCC_DeInit();
-
-	//2. update the SystemCoreClock variable
-	SystemCoreClockUpdate();
-
-	prvSetupHardware();
-
-
-	sprintf(usr_msg,"This is hello-world application starting\r\n");
-	printmsg(usr_msg);
-
-	//Start Recording
-	SEGGER_SYSVIEW_Conf();
-	SEGGER_SYSVIEW_Start();
-
-	//3. lets create 2 tasks , task-1 and task-2
-	xTaskCreate( vTask1_handler,"Task-1", configMINIMAL_STACK_SIZE,NULL,2,&xTaskHandle1 );
-
-	xTaskCreate( vTask2_handler,"Task-2", configMINIMAL_STACK_SIZE,NULL,2,&xTaskHandle2 );
-
-    //4. Start the scheduler.
-	vTaskStartScheduler();
-
-
-	//you will never return here
-	for(;;);
-}
-
-
-
-void vTask1_handler(void *params)
-{
-	//traceTASK_SWITCHED_IN();
-	while(1)
-	{
-		if(UART_ACCESS_KEY == AVAILABLE)
-		{
-			UART_ACCESS_KEY = NOT_AVAILABLE;
-			printmsg("Hello-world: From Task-1\r\n");
-			UART_ACCESS_KEY = AVAILABLE;
-			SEGGER_SYSVIEW_Print("Task1 is yielding");
-			traceISR_EXIT_TO_SCHEDULER();
-			taskYIELD();
-		}
-
-	}
-
-}
-
-
-
-void vTask2_handler(void *params)
-{
-	while(1)
-	{
-		if(UART_ACCESS_KEY == AVAILABLE)
-		{
-			UART_ACCESS_KEY = NOT_AVAILABLE;
-			printmsg("Hello-world: From Task-2\r\n");
-			UART_ACCESS_KEY = AVAILABLE;
-			SEGGER_SYSVIEW_Print("Task2 is yielding");
-			traceISR_EXIT_TO_SCHEDULER();
-			taskYIELD();
-		}
-	}
-}
-
-static void prvSetupUart(void)
-{
-	GPIO_InitTypeDef gpio_uart_pins;
-	USART_InitTypeDef uart2_init;
-
-	//1. Enable the UART2  and GPIOA Peripheral clock
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
-
-	//PA2 is UART2_TX, PA3 is UART2_RX
-
-	//2. Alternate function configuration of MCU pins to behave as UART2 TX and RX
-
-	//zeroing each and every member element of the structure
-	memset(&gpio_uart_pins,0,sizeof(gpio_uart_pins));
-
-	gpio_uart_pins.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-	gpio_uart_pins.GPIO_Mode = GPIO_Mode_AF;
-	gpio_uart_pins.GPIO_PuPd = GPIO_PuPd_UP;
-	gpio_uart_pins.GPIO_OType= GPIO_OType_PP;
-	gpio_uart_pins.GPIO_Speed = GPIO_High_Speed;
-	GPIO_Init(GPIOA, &gpio_uart_pins);
-
-
-	//3. AF mode settings for the pins
-    GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_USART2); //PA2
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_USART2); //PA3
-
-	//4. UART parameter initializations
-	//zeroing each and every member element of the structure
-	memset(&uart2_init,0,sizeof(uart2_init));
-
-	uart2_init.USART_BaudRate = 115200;
-	uart2_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	uart2_init.USART_Mode =  USART_Mode_Tx | USART_Mode_Rx;
-	uart2_init.USART_Parity = USART_Parity_No;
-	uart2_init.USART_StopBits = USART_StopBits_1;
-	uart2_init.USART_WordLength = USART_WordLength_8b;
-	USART_Init(USART2,&uart2_init);
-
-
-	//5. Enable the UART2 peripheral
-	USART_Cmd(USART2,ENABLE);
-
-}
-
-static void prvSetupHardware(void)
-{
-	//setup UART2
-	prvSetupUart();
-}
 
 void printmsg(char *msg)
 {
-	for(uint32_t i=0; i < strlen(msg); i++)
+	for(uint32_t i=0;i<strlen(msg);i++)
 	{
-		while ( USART_GetFlagStatus(USART2,USART_FLAG_TXE) != SET);
+		while(USART_GetFlagStatus(USART2,USART_FLAG_TXE)!=SET);
 		USART_SendData(USART2,msg[i]);
 	}
-
-	while ( USART_GetFlagStatus(USART2,USART_FLAG_TC) != SET);
-
 }
+char msg[250];
+int main(void)
+{
+#ifdef USE_SEMIHOSTING
+	initialise_monitor_handles();
+	printf("Hello Dai Nguyen");
+#endif
+	DWT->CTRL|=(1<<0);
+	RCC_DeInit();
+	SystemCoreClockUpdate();
+	prvSetupHardware();
+
+	sprintf(msg,"hello Dai Nguyen aaa\n");
+	printmsg(msg);
+	SEGGER_SYSVIEW_Conf();
+	SEGGER_SYSVIEW_Start();
+	xSemaphore = xSemaphoreCreateBinary();
+
+	if(xSemaphore!=NULL)
+	{
+		xSemaphoreGive(xSemaphore);
+
+		xTaskCreate(vTask1,"Task 1",configMINIMAL_STACK_SIZE,NULL,1,&xTaskHandle_1);//
+		xTaskCreate(vTask2,"Task 2",configMINIMAL_STACK_SIZE,NULL,1,&xTaskHandle_2);//
+		vTaskStartScheduler();
+	}
+}
+
+
+void vTask1(void *params)
+{
+	while(1)
+	{
+		xSemaphoreTake(xSemaphore, portMAX_DELAY );
+		{
+			sprintf(msg,"hello from Task 1\n");
+			printmsg(msg);
+			xSemaphoreGive(xSemaphore);
+		}
+
+		vTaskDelay(500/portTICK_RATE_MS);
+		//taskYIELD();///
+
+	};
+}
+
+void vTask2(void *params)
+{
+	while(1)
+	{
+		xSemaphoreTake(xSemaphore, portMAX_DELAY );
+		{
+			sprintf(msg,"hello from Task 2\n");
+			printmsg(msg);
+			xSemaphoreGive(xSemaphore);
+		}
+		vTaskDelay(500/portTICK_RATE_MS);
+		//taskYIELD();///
+	};
+}
+static void prvSetupUSART(void)
+{
+	GPIO_InitTypeDef gpio_uart_pins;
+	USART_InitTypeDef uart2_init;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+
+	// zeroing every elements of the structure
+	memset(&gpio_uart_pins,0,sizeof(gpio_uart_pins));
+	//PA2: RX; Pa3: RX
+
+	//void GPIO_Init(GPIO_TypeDef* GPIOx, GPIO_InitTypeDef* GPIO_InitStruct)
+	gpio_uart_pins.GPIO_Pin=GPIO_Pin_2|GPIO_Pin_3;
+	gpio_uart_pins.GPIO_Mode=GPIO_Mode_AF;
+	gpio_uart_pins.GPIO_PuPd=GPIO_PuPd_UP;
+	gpio_uart_pins.GPIO_Speed = GPIO_High_Speed;
+
+	GPIO_Init(GPIOA,&gpio_uart_pins );
+	// AF mode setting
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_USART2);// TX
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_USART2);// TX
+
+	// Uart parameter init
+	// USART_Init(USART_TypeDef* USARTx, USART_InitTypeDef* USART_InitStruct)
+	memset(&uart2_init,0,sizeof(uart2_init));
+	uart2_init.USART_BaudRate=115200;
+	uart2_init.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
+	uart2_init.USART_Mode=USART_Mode_Tx|USART_Mode_Rx;
+	uart2_init.USART_Parity=USART_Parity_No;
+	uart2_init.USART_StopBits=USART_StopBits_1;
+	uart2_init.USART_WordLength=USART_WordLength_8b;
+	USART_Init(USART2,&uart2_init);
+	USART_Cmd(USART2,ENABLE);
+	}
+
+static void prvSetupHardware(void)
+{
+	prvSetupUSART();
+
+	}
